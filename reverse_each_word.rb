c67879b6ee1689c1db37ee47f35a04145078172a
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2019-08-11 20:56:10 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,23 @@
+def reverse_each_word(sentence)
+  new_array = sentence.split(" ")
+  array = []
+  new_array.each do |word|
+  array << word.reverse
+  
+  
+  end
+  array.join(" ")
+end
+
+
+
+def reverse_each_word(sentence)
+  new_array = sentence.split(" ")
+  array = []
+  new_array.collect do |word|
+  array << word.reverse
+  
+  
+  end
+  array.join(" ")
+end
