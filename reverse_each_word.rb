e888ef72615d3103f63f2bea5a2089fecaf6c3c4
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2019-08-11 20:47:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,3 +11,13 @@ end
 
 
 
+ef reverse_each_word(sentence)
+  new_array = sentence.split(" ")
+  array = []
+  new_array.collect do |word|
+  array << word.reverse
+  
+  
+  end
+  array.join(" ")
+end
